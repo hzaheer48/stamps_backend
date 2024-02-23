de5aa6bc6e41b34ea211a868bcef9ee81b9dbbd4
Hammad Zaheer <83864807+hzaheer48@users.noreply.github.com>
2024-02-23 12:40:00 +0500

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './typeorm/entities/User';
 import { UsersModule } from './users/users.module';
-import { NestjsFormDataModule } from 'nestjs-form-data';
 import { AuthModule } from './auth/auth.module';
 import { Product } from './typeorm/entities/Product';
 import { ShoppingCart } from './typeorm/entities/ShoppingCart';
@@ -13,10 +12,10 @@ import { CartItem } from './typeorm/entities/CartItem';
 @Module({
   imports: [TypeOrmModule.forRoot({
     type:'mysql',
-    host:'localhost',
+    host:'primary.stamps--7bwdbvpvpfsm.addon.code.run',
     port:3306,
-    username:'root',
-    password:'',
+    username:'ef36d5b002cc240e',
+    password:'282ff37c4335f51cf1b7d96aca1059',
     database:'stamps',
     entities:[User,Product,ShoppingCart,CartItem],
     synchronize:true,
